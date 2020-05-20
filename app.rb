7d9ac5b5f6e7a1333c580dead0d62f1efbdc2fda
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-20 17:17:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Yvonne."
+  end
+
+  get '/hometown' do
+    "My hometown is San Jose."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is anything."
+  end
+
 end
